31938152f703f6bd8ba57e8569a1d6e31efb7f83
marcobusacca <59514839+marcobusacca@users.noreply.github.com>
2023-09-13 10:56:39 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    components: {
+        AppHeader,
+    },
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+    <AppHeader/>
+    <main>
+        <router-view></router-view>
+    </main>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use './styles/generals.scss' as *;
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<!-- JAVASCRIPT & VUE.JS -->
+<script>
+import { store } from '../store';
+
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<!-- TEMPLATE HTML -->
+<template lang="">
+    <div class="container mb-5">
+        <div class="row">
+            <div class="col-12">
+                <h1>Homepage</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- STYLE SCSS -->
+<style lang="scss">
+// IMPORTO GENERALS.SCSS
+@use '../styles/generals.scss' as *;
+</style>

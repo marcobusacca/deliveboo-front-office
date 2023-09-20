2a97c691f49423f1597900a78e91e45213d0b62a
antoniogisondi <anto.gisondi@tiscali.it>
2023-09-20 12:37:44 +0200

    [STYLE] Slidet restaurant types

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,7 +7,53 @@ export default {
         return {
             currentSlideIndex: 0,
             store,
-            types: []
+            types: [],
+            types_info: [
+                {
+                    title: 'Trattoria',
+                    description: 'Una trattoria è un\'accogliente osteria italiana che serve piatti casalinghi preparati con ingredienti freschi e di alta qualità. Qui puoi gustare autentiche specialità regionali, come pasta fatta in casa, risotti e arrosti tradizionali.'
+                },
+                {
+                    title: 'Messicano',
+                    description: 'Un ristorante messicano ti porta in un viaggio culinario attraverso i sapori audaci e speziati del Messico. Dai tacos agli enchiladas, ogni piatto è una festa di colori e sapori autentici, spesso accompagnati da guacamole e salse fatte in casa.'
+                },
+                {
+                    title: 'Pizzeria',
+                    description: 'Una pizzeria tradizionale è il luogo perfetto per gli amanti della pizza autentica italiana. Le pizze sono cotte in forni a legna, garantendo una crosta croccante e un sapore irresistibile. Scegli tra una vasta selezione di gusti e ingredienti freschi.'
+                },
+                {
+                    title: 'Steakhouse',
+                    description: 'Una steakhouse di alta qualità è il paradiso per gli appassionati di carne. Qui puoi gustare succulente bistecche preparate con cura, selezionate tra i tagli migliori e accompagnate da contorni deliziosi e salse esclusive.'
+                },
+                {
+                    title: 'Cinese',
+                    description: 'Un ristorante cinese autentico ti offre un\'esperienza culinaria che abbraccia la diversità della cucina cinese. Dalle delicate dim sum ai piatti principali come l\'anatra alla pechinese, ogni portata è un viaggio attraverso le tradizioni gustative della Cina.'
+                },
+                {
+                    title: 'Sushi',
+                    description: 'Il nostro sushi bar raffinato offre un\'ampia selezione di prelibatezze giapponesi. Ogni pezzo di sushi e sashimi è preparato con maestria, utilizzando solo i migliori ingredienti freschi e serviti con arte e precisione.'
+                },
+                {
+                    title: 'Fast Food',
+                    description: 'Il nostro fast food gourmet offre una versione raffinata dei tuoi piatti preferiti. Qui puoi gustare hamburger artigianali, patatine fritte croccanti e altri deliziosi comfort food, preparati con ingredienti di alta qualità.'
+                },
+                {
+                    title: 'Gelateria',
+                    description: 'La nostra gelateria artigianale è il posto ideale per gli amanti del gelato. Ogni gusto è preparato con cura utilizzando ingredienti freschi e genuini. Scegli tra una varietà di gusti irresistibili e goditi il vero sapore dell\'Italia.'
+                },
+                {
+                    title: 'Kebab',
+                    description: ' Il nostro ristorante kebab fusion offre un\'interpretazione creativa dei piatti tradizionali del Medio Oriente. Le pietanze sono preparate con ingredienti freschi e spezie aromatiche, creando sapori unici e appaganti.'
+                },
+                {
+                    title: 'Pasticceria',
+                    description: 'La nostra pasticceria artigianale ti delizia con una selezione di dolci prelibati e prodotti da forno freschi di giornata. Da torte decorate a pasticcini deliziosi, ogni dolce è un capolavoro di gusto e creatività.'
+                },
+                {
+                    title: 'Piadineria',
+                    description: 'La nostra piadineria contemporanea offre un\'interpretazione moderna di un classico italiano. Le piadine sono preparate al momento con ingredienti freschi e riempite con una varietà di gustosi condimenti, creando un\'esperienza culinaria unica.'
+                }
+            ]
         }
     },
     created() {
@@ -17,10 +63,18 @@ export default {
         getRestaurantTypes() {
             axios.get(`${store.baseUrl}/api/types`).then((response) => {
                 this.types = response.data.results;
+
             })
+            console.log(this.types)
         },
         nextSlide() {
             if (this.currentSlideIndex < this.types.length - 1) {
+                this.currentSlideIndex++;
+            } else {
+                this.currentSlideIndex = 0;
+            }
+
+            if (this.currentSlideIndex < this.types_info.length - 1) {
                 this.currentSlideIndex++;
             } else {
                 this.currentSlideIndex = 0;
@@ -31,6 +85,12 @@ export default {
                 this.currentSlideIndex--;
             } else {
                 this.currentSlideIndex = this.types.length - 1;
+            }
+
+            if (this.currentSlideIndex > 0) {
+                this.currentSlideIndex--;
+            } else {
+                this.currentSlideIndex = this.types_info.length - 1;
             }
         }
     },
@@ -77,10 +137,15 @@ export default {
                 </div>
             </div> -->
             <div class="row justify-content-center">
-                <div class="card w-75 px-0">
+                <div class="card w-50 px-0">
                     <div class="col-12 d-flex">
                         <div class="col-4 bg-left">
-
+                            <div v-for="(item, index) in types_info" :key="index" class="type-info">
+                                <div v-if="index === currentSlideIndex">
+                                    <h3 class="type-title">{{ item.title }}</h3>
+                                    <p class="type-description">{{ item.description }}</p>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="col-8 col-image">
@@ -101,9 +166,6 @@ export default {
                                     <div class="border-0">
                                         <img :src="`${store.baseUrl}/storage/${type.cover_image}`" class="img-fluid"
                                             alt="logo">
-                                        <!-- <div class="card-body bg-orange">
-                                            <h6 class="card-text text-center bg-white">{{ type.name }}</h6>
-                                        </div> -->
                                     </div>
                                 </div>
                             </div>
@@ -114,28 +176,6 @@ export default {
         </div>
     </main>
 </template>
-<!-- <div class="d-flex justify-content-end align-items-center">
-    <button @click="prevSlide">
-        
-    </button>
-</div>
-<div class="d-flex justify-content-center align-items-center">
-    <div v-for="(type, index) in types" :key="index">
-        <div class="col-12 d-flex flex-column justify-content-center align-items-center"
-            v-if="index === currentSlideIndex">
-            <div class="card border-0 my-3 card-size">
-                <img :src="`${store.baseUrl}/storage/${type.cover_image}`" class="card-img-top"
-                    alt="logo">
-                <div class="card-body bg-orange">
-                    <h6 class="card-text text-center bg-white">{{ type.name }}</h6>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="d-flex justify-content-center align-items-center">
-    <button @click="nextSlide">Next</button>
-</div> -->
 
 <!-- STYLE SCSS -->
 <style lang="scss">
@@ -143,10 +183,23 @@ export default {
 @use '../styles/generals.scss' as *;
 
 .bg-left {
-    width: 100%;
-    height: 100%;
     background-color: orange;
+    text-align: center;
+    padding: 15px;
 }
+
+.type-title {
+    font-size: 1.5em;
+    font-weight: bold;
+    color: #333;
+}
+
+.type-description {
+    font-size: 1em;
+    line-height: 1.6;
+    color: #555;
+}
+
 
 .col-image {
     position: relative;

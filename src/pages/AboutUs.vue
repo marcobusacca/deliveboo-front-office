8283c37a29de5f28299e6ee1d3fcd001dece4b0e
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-09-21 17:54:50 +0200

    fix

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -4,23 +4,93 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        <div class="container">
+    <div class="sfondo">
+        <div class="container mb-3 mt-3">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center">Chi siamo</h1>
+                    <h1 class="m-4" ><strong>Chi siamo</strong></h1>
                 </div>
             </div>
         </div>
-        <div class="container">
+        <div class="container mt-3">
             <div class="row">
-                <div class="col-12">
-                    <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod ipsum unde corporis inventore eos aliquam odio neque mollitia dicta architecto maxime vel porro veritatis, natus omnis deleniti pariatur iusto animi.</p>
+                <div class=" col-6  ">
+                    <p class="blocco p-2">Quando il fondatore e CEO di Deliveroo, Will Shu, si è trasferito a Londra nel 2013, ha scoperto una città piena di ottimi ristoranti, ma è rimasto stupito dal fatto che così pochi di loro consegnassero cibo. La sua missione personale è stata quella di portare i migliori ristoranti locali direttamente alle porte delle persone.
+
+                    Oggi, Deliveroo gestisce un mercato iperlocale trilaterale, che collega consumatori locali, ristoranti, negozi di alimentari e ciclisti per realizzare un acquisto emotivo e mission-critical in meno di 30 minuti. Offrendo consegne rapide e affidabili che i consumatori possono monitorare online, Deliveroo è cresciuto rapidamente. Deliveroo ora opera in 10 mercati in tutto il mondo, collaborando con migliaia di ristoranti e negozi di alimentari, migliaia di passeggeri e servendo milioni di consumatori.z</p>
+
+                </div>
+                <div class="col-6 ">
+                   <h2 class="text-center"><strong>la nostra storia</strong></h2>
+                  
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-6  ">
+                    <h2 class="text-center"><strong>Consumatori</strong></h2>
+                    
+
+                </div>
+                <div class="col-6 ">
+                  
+                   <p class="blocco p-4">La consegna del cibo è un business iperlocale. Pensiamo alla nostra offerta ai consumatori quartiere per quartiere.
+
+                Abbiamo trascorso anni lavorando per capire cosa vogliono i consumatori e per stabilire una proposta che possa essere adattata per attrarre ogni quartiere e consumatore: dagli studenti, ai professionisti o alle famiglie, e dai centri città alle periferie, alle cinture dei pendolari e alle piccole città. Tutti mangiano e Deliveroo ha un'offerta che riflette ciò che vogliono le persone in ogni quartiere.
+
+                La nostra proposta ai consumatori mira a fornire sempre la migliore disponibilità, selezione, esperienza e valore, costruendo allo stesso tempo una connessione emotiva con il nostro marchio.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-6  ">
+                    <p class="blocco p-4">Dal 2013 siamo cresciuti fino a collaborare con 160.000 ristoranti e supermercati partner.
+
+                    Abbiamo iniziato come servizio di consegna di cibo per ristoranti, collaborando con catene di ristoranti nazionali e con i preferiti locali indipendenti. Il nostro servizio ha sbloccato la consegna per oltre il 90% dei partner sulla nostra piattaforma, aziende che non avevano mai offerto la consegna in precedenza. Il nostro modello ha ampliato notevolmente il numero di ristoranti che possono offrire la consegna e di conseguenza ha aperto nuove opzioni alimentari per i consumatori di tutto il mondo.
+
+                    Nel 2018, data la nostra ambizione di costruire l'azienda alimentare online definitiva, abbiamo iniziato ad aggiungere partner di generi alimentari alla nostra piattaforma con un servizio di generi alimentari on-demand basato sulla stessa tecnologia che guida il nostro principale mercato di ristoranti abilitati alla logistica. Oggi collaboriamo con alcuni dei più grandi rivenditori di generi alimentari al mondo, tra cui Co-op, Conad, Carrefour, Casino, Marks and Spencer, Picard, Waitrose e Whole Foods Market.</p>
+                    
+
+                </div>
+                <div class="col-6 ">
+                    <h2 class="text-center"><strong>Ristoranti e negozi di alimentari</strong></h2>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-6  ">
+                    <h2 class="text-center">Consumatori</h2>
+                    
+
+                </div>
+                <div class="col-6 ">
+                  
+                   <p class="blocco p-4">La consegna del cibo è un business iperlocale. Pensiamo alla nostra offerta ai consumatori quartiere per quartiere.
+
+                Abbiamo trascorso anni lavorando per capire cosa vogliono i consumatori e per stabilire una proposta che possa essere adattata per attrarre ogni quartiere e consumatore: dagli studenti, ai professionisti o alle famiglie, e dai centri città alle periferie, alle cinture dei pendolari e alle piccole città. Tutti mangiano e Deliveroo ha un'offerta che riflette ciò che vogliono le persone in ogni quartiere.
+
+                La nostra proposta ai consumatori mira a fornire sempre la migliore disponibilità, selezione, esperienza e valore, costruendo allo stesso tempo una connessione emotiva con il nostro marchio.</p>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style lang="">
+<style lang="scss">
+    .sfondo{
+        background-image:url('../assets/aboutsus/aboutus.png') ;
+        max-height: 100vh;
+        
+    }
+    .blocco{
+        background-color: white;
+    }
+    .container{
+        max-width: 70%;
+
+    }
     
 </style>

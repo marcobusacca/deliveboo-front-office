5942d9ef1ea602ab505139f90547bbbe422f9c60
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-09-22 16:45:23 +0200

    finito

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -13,7 +13,7 @@ export default {
         <div class="about-1">
             <h1>CHI SIAMO</h1>
             <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta laborum, animi excepturi quam alias dolorum in, rem cupiditate corrupti atque porro, laboriosam praesentium suscipit illo voluptates beatae recusandae magnam aliquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloremque animi atque praesentium sunt, numquam excepturi mollitia deserunt modi. Pariatur rem ratione libero. Magni recusandae nam minima dolorum magnam quibusdam!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, quo optio ad deserunt error provident ipsa ut voluptatum iste maxime, quisquam consectetur, dolorem similique molestias corrupti perferendis minus nulla illo.
+                In Deliveroo i nostri valori sono al centro di tutto ciò che facciamo. Sono la nostra cultura, la nostra mentalità e l’espressione di ciò che siamo come persone e come azienda quando diamo il meglio di noi. Considerateli come i nostri principi guida. Stabiliscono gli standard su come facciamo le cose, dal modo in cui ci parliamo, alle decisioni che prendiamo e persino alle persone che introduciamo nel team. In parole povere, sono la nostra tabella di marcia per ciò che è possibile.
             </p>
         </div>
         <div id="about-2">
@@ -25,8 +25,9 @@ export default {
                                 <i class="fa-solid fa-timeline"></i>
                                 <h3>LA NOSTRA STORIA</h3>
                                 <hr>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi?
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id doloremque, nostrum at ex assumenda doloribus quisquam numquam nam molestias rem perspiciatis delectus ducimus sint eius est recusandae facilis laboriosam. Possimus!
+                                <p>Quando il fondatore e CEO di Deliveroo, Will Shu, si è trasferito a Londra nel 2013, ha scoperto una città piena di ottimi ristoranti, ma è rimasto stupito dal fatto che così pochi di loro consegnassero cibo. La sua missione personale è stata quella di portare i migliori ristoranti locali direttamente alle porte delle persone.
+
+                                Oggi, Deliveroo gestisce un mercato iperlocale trilaterale, che collega consumatori locali, ristoranti, negozi di alimentari e ciclisti per realizzare un acquisto emotivo e mission-critical in meno di 30 minuti. Offrendo consegne rapide e affidabili che i consumatori possono monitorare online, Deliveroo è cresciuto rapidamente. Deliveroo ora opera in 10 mercati in tutto il mondo, collaborando con migliaia di ristoranti e negozi di alimentari, migliaia di passeggeri e servendo milioni di consumatori.
                                 </p>
                             </div>
                         </div>
@@ -35,8 +36,11 @@ export default {
                                 <i class="fa-solid fa-users"></i>
                                 <h3>CONSUMATORI</h3>
                                 <hr>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi?
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, atque exercitationem commodi fugiat ratione porro. Nulla adipisci beatae, aut cum dolorum rerum consequatur accusamus. Nostrum debitis tenetur nulla quos facilis.
+                                <p>La consegna del cibo è un business iperlocale. Pensiamo alla nostra offerta ai consumatori quartiere per quartiere.
+
+                                    Abbiamo trascorso anni lavorando per capire cosa vogliono i consumatori e per stabilire una proposta che possa essere adattata per attrarre ogni quartiere e consumatore: dagli studenti, ai professionisti o alle famiglie, e dai centri città alle periferie, alle cinture dei pendolari e alle piccole città. Tutti mangiano e Deliveroo ha un'offerta che riflette ciò che vogliono le persone in ogni quartiere.
+
+                                    La nostra proposta ai consumatori mira a fornire sempre la migliore disponibilità, selezione, esperienza e valore, costruendo allo stesso tempo una connessione emotiva con il nostro marchio.
                                 </p>
                             </div>
                         </div>
@@ -45,8 +49,11 @@ export default {
                                 <i class="fa-solid fa-person-biking"></i>
                                 <h3>RIDER</h3>
                                 <hr>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi? 
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, accusantium? Itaque vitae modi delectus odio! Atque beatae aut amet natus esse accusamus nisi deleniti sed dolorum quia! Accusamus, fugit voluptas loren max for.</p>
+                                <p>Costruiamo la nostra proposta per i ciclisti in base alle cose che ci dicono di apprezzare di più.
+
+                                    Questa comprensione di ciò che sta più a cuore ai ciclisti ci ha aiutato a sviluppare un’offerta che dà priorità alle cose a cui tengono: lavoro flessibile, buoni guadagni e sicurezza. Deliveroo è stata tra le prime piattaforme di consegna di cibo a fornire un'assicurazione gratuita contro gli infortuni personali e la responsabilità civile a tutti i passeggeri autonomi a livello globale. Questa copertura è fornita gratuitamente e protegge i passeggeri fin dal loro primo ordine con Deliveroo.
+
+                                    Il nostro fondatore e CEO, Will, è stato il primo corriere e ha lavorato a tempo pieno nelle consegne per il primo anno di gestione dell'attività. Insieme ai membri del team di Deliveroo, continua ancora oggi a completare le consegne regolarmente.</p>
                             </div>
                         </div>
                     </div>
@@ -63,7 +70,7 @@ export default {
 <style lang="scss">
     #about{
         background:url(../assets/aboutsus/aboutus.png);
-        height: 100vh;
+    
     }
    .about-1{
     margin: 30px;
@@ -80,6 +87,7 @@ export default {
     }
    }
    .about-item{
+    min-height: 600px;
     margin-bottom: 20px;
     margin-top: 20px;
     background-color: #fff;

a6941614afb23262b644388c7ff669578dc8935b
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-09-22 13:19:34 +0200

    aboutus finito

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,96 +1,124 @@
 <script>
+import AppFooter from '../components/AppFooter.vue'
 export default {
-    name:'AboutUS'
+    name:'AboutUS',
+     components: {
+        
+        AppFooter
+    },
 }
 </script>
 <template lang="">
-    <div class="sfondo">
-        <div class="container mb-3 mt-3">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="m-4" ><strong>Chi siamo</strong></h1>
+    <section id="about">
+        <div class="about-1">
+            <h1>CHI SIAMO</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta laborum, animi excepturi quam alias dolorum in, rem cupiditate corrupti atque porro, laboriosam praesentium suscipit illo voluptates beatae recusandae magnam aliquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus doloremque animi atque praesentium sunt, numquam excepturi mollitia deserunt modi. Pariatur rem ratione libero. Magni recusandae nam minima dolorum magnam quibusdam!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, quo optio ad deserunt error provident ipsa ut voluptatum iste maxime, quisquam consectetur, dolorem similique molestias corrupti perferendis minus nulla illo.
+            </p>
+        </div>
+        <div id="about-2">
+            <div class="content-box-lg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="about-item text-center">
+                                <i class="fa-solid fa-timeline"></i>
+                                <h3>LA NOSTRA STORIA</h3>
+                                <hr>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi?
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id doloremque, nostrum at ex assumenda doloribus quisquam numquam nam molestias rem perspiciatis delectus ducimus sint eius est recusandae facilis laboriosam. Possimus!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="about-item text-center">
+                                <i class="fa-solid fa-users"></i>
+                                <h3>CONSUMATORI</h3>
+                                <hr>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi?
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum, atque exercitationem commodi fugiat ratione porro. Nulla adipisci beatae, aut cum dolorum rerum consequatur accusamus. Nostrum debitis tenetur nulla quos facilis.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="about-item text-center">
+                                <i class="fa-solid fa-person-biking"></i>
+                                <h3>RIDER</h3>
+                                <hr>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos debitis inventore recusandae numquam sequi officiis nesciunt culpa soluta pariatur! Quos quam dignissimos quo. Fugiat, ratione placeat dicta nemo sapiente quasi? 
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique, accusantium? Itaque vitae modi delectus odio! Atque beatae aut amet natus esse accusamus nisi deleniti sed dolorum quia! Accusamus, fugit voluptas loren max for.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="container mt-3">
-            <div class="row">
-                <div class=" col-6  ">
-                    <p class="blocco p-2">Quando il fondatore e CEO di Deliveroo, Will Shu, si è trasferito a Londra nel 2013, ha scoperto una città piena di ottimi ristoranti, ma è rimasto stupito dal fatto che così pochi di loro consegnassero cibo. La sua missione personale è stata quella di portare i migliori ristoranti locali direttamente alle porte delle persone.
-
-                    Oggi, Deliveroo gestisce un mercato iperlocale trilaterale, che collega consumatori locali, ristoranti, negozi di alimentari e ciclisti per realizzare un acquisto emotivo e mission-critical in meno di 30 minuti. Offrendo consegne rapide e affidabili che i consumatori possono monitorare online, Deliveroo è cresciuto rapidamente. Deliveroo ora opera in 10 mercati in tutto il mondo, collaborando con migliaia di ristoranti e negozi di alimentari, migliaia di passeggeri e servendo milioni di consumatori.z</p>
-
-                </div>
-                <div class="col-6 ">
-                   <h2 class="text-center"><strong>la nostra storia</strong></h2>
-                  
-                </div>
-            </div>
-        </div>
-        <div class="container mt-3">
-            <div class="row">
-                <div class="col-6  ">
-                    <h2 class="text-center"><strong>Consumatori</strong></h2>
-                    
-
-                </div>
-                <div class="col-6 ">
-                  
-                   <p class="blocco p-4">La consegna del cibo è un business iperlocale. Pensiamo alla nostra offerta ai consumatori quartiere per quartiere.
-
-                Abbiamo trascorso anni lavorando per capire cosa vogliono i consumatori e per stabilire una proposta che possa essere adattata per attrarre ogni quartiere e consumatore: dagli studenti, ai professionisti o alle famiglie, e dai centri città alle periferie, alle cinture dei pendolari e alle piccole città. Tutti mangiano e Deliveroo ha un'offerta che riflette ciò che vogliono le persone in ogni quartiere.
-
-                La nostra proposta ai consumatori mira a fornire sempre la migliore disponibilità, selezione, esperienza e valore, costruendo allo stesso tempo una connessione emotiva con il nostro marchio.</p>
-                </div>
-            </div>
-        </div>
-        <div class="container mt-3">
-            <div class="row">
-                <div class="col-6  ">
-                    <p class="blocco p-4">Dal 2013 siamo cresciuti fino a collaborare con 160.000 ristoranti e supermercati partner.
-
-                    Abbiamo iniziato come servizio di consegna di cibo per ristoranti, collaborando con catene di ristoranti nazionali e con i preferiti locali indipendenti. Il nostro servizio ha sbloccato la consegna per oltre il 90% dei partner sulla nostra piattaforma, aziende che non avevano mai offerto la consegna in precedenza. Il nostro modello ha ampliato notevolmente il numero di ristoranti che possono offrire la consegna e di conseguenza ha aperto nuove opzioni alimentari per i consumatori di tutto il mondo.
-
-                    Nel 2018, data la nostra ambizione di costruire l'azienda alimentare online definitiva, abbiamo iniziato ad aggiungere partner di generi alimentari alla nostra piattaforma con un servizio di generi alimentari on-demand basato sulla stessa tecnologia che guida il nostro principale mercato di ristoranti abilitati alla logistica. Oggi collaboriamo con alcuni dei più grandi rivenditori di generi alimentari al mondo, tra cui Co-op, Conad, Carrefour, Casino, Marks and Spencer, Picard, Waitrose e Whole Foods Market.</p>
-                    
-
-                </div>
-                <div class="col-6 ">
-                    <h2 class="text-center"><strong>Ristoranti e negozi di alimentari</strong></h2>
-                   
-                </div>
-            </div>
-        </div>
-        <div class="container mt-3">
-            <div class="row">
-                <div class="col-6  ">
-                    <h2 class="text-center">Consumatori</h2>
-                    
-
-                </div>
-                <div class="col-6 ">
-                  
-                   <p class="blocco p-4">La consegna del cibo è un business iperlocale. Pensiamo alla nostra offerta ai consumatori quartiere per quartiere.
-
-                Abbiamo trascorso anni lavorando per capire cosa vogliono i consumatori e per stabilire una proposta che possa essere adattata per attrarre ogni quartiere e consumatore: dagli studenti, ai professionisti o alle famiglie, e dai centri città alle periferie, alle cinture dei pendolari e alle piccole città. Tutti mangiano e Deliveroo ha un'offerta che riflette ciò che vogliono le persone in ogni quartiere.
-
-                La nostra proposta ai consumatori mira a fornire sempre la migliore disponibilità, selezione, esperienza e valore, costruendo allo stesso tempo una connessione emotiva con il nostro marchio.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
+    <footer>
+        <AppFooter/>
+    </footer>
 </template>
+.
+    
 <style lang="scss">
-    .sfondo{
-        background-image:url('../assets/aboutsus/aboutus.png') ;
-        max-height: 100vh;
+    #about{
+        background:url(../assets/aboutsus/aboutus.png);
+        height: 100vh;
+    }
+   .about-1{
+    margin: 30px;
+    padding: 5px;
+    h1{
+        text-align: center;
+        color: black;
+        font-weight: bold;
+    }
+    p{
+        text-align: center;
+        padding: 3px;
         
     }
-    .blocco{
-        background-color: white;
+   }
+   .about-item{
+    margin-bottom: 20px;
+    margin-top: 20px;
+    background-color: #fff;
+    padding-top: 50px;
+    padding-bottom: 30px;
+    box-shadow: 0 0 9px rgb(9, 100, 13);
+    i{
+        font-size: 50px;
+        margin: 0;
+        margin-bottom: 20px;
     }
-    .container{
-        max-width: 70%;
-
+    h3{
+        font-size: 25px;
+        margin-bottom: 20px;
+    }
+    hr{
+        width: 46px;
+        height: 3px;
+        background-color: rgb(9, 100, 13);
+        margin: 0 auto;
+        border: none;
+    }
+    p{
+        margin-top: 20px;
     }
     
+   }
+   .about-item:hover{
+        background-color:rgb(9, 100, 13) ;
+    }
+    .about-item:hover i,
+    .about-item:hover h3,
+    .about-item:hover p{
+        color: #fff;
+    }
+    .about-item:hover hr{
+        background-color: white;
+    }
+
+    
+
 </style>

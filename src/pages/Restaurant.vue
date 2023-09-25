dd5591283df2c74842c2d04a43155ef4988176c6
andreathepa <andreadepa32@gmail.com>
2023-09-25 16:33:28 +0200

    [FEAT] Mobile cartel

--- src/pages/Restaurant.vue
+++ src/pages/Restaurant.vue
@@ -5,68 +5,18 @@ export default {
 </script>
 
 <template lang="">
-    
-    <!-- <div class="container-restaurant">
-        <div class="image-restaurant">
-            <img src="../assets/bacon.jpg" class="img-fluid restaurant-back" alt="">
-
-        </div>
-        <div class="container-fluid bg-white p-5">
-            <div class="row">
-                <div class="col-12">
-                    <div class="content-restaurant card">
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-                        <div>ciaone</div>
-    
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
+    <!-- Copertina ristorante -->
     <div class="container-restaurant position-relative">
         <div class="image-restaurant">
             
         </div>
     </div>
+    <!-- Container menù ristorante -->
     <div class="container container-menù shadow mb-5">
         <div class="row">
             <div class="col-12 col-md-9">
                 <div class="p-3 text-center mt-3 mb-3">
+                    <!-- Dettagli ristorante -->
                     <div class="d-flex justify-content-center">
                         <img src="../assets/logo-ristorante.png" class="img-fluid logo-ristorante" alt="">
                     </div>
@@ -75,6 +25,7 @@ export default {
                 </div>
                 <div class="container">
                     <div class="row justify-content-center">
+                        <!-- Piatti del menù -->
                         <div class="col-12 col-md-4 col-lg-3 mx-2 my-2 card p-3 bg-alert">
                             <div class="card border-0">
                                 <div class="card-header p-0 border-0">
@@ -196,8 +147,9 @@ export default {
                     </div>
                 </div>
             </div>
+            <!-- Colonna carrello tablet e pc -->
             <div class="col-12 col-md-3">
-                <div class="carrello p-3 py-5 text-center text-white">
+                <div class="carrello p-3 py-5 text-center text-white d-none d-md-block">
                     <div class="card">
                         <h1>Il tuo carrello</h1>
                         <p>ordine 1</p>
@@ -210,7 +162,87 @@ export default {
             
         </div>
     </div>
-
+    <!-- Carrello versione mobile -->
+    <div class="container p-3 sticky-bottom bg-white d-block d-sm-none">
+        <div class="row">
+            <div class="col-12 card p-3">
+                <div class="cartel-mobile mb-2">
+                    <span class="badge bg-primary rounded-pill px-2"><i class="fa-solid fa-basket-shopping px-2"></i>14</span>
+                    <span class="px-2">Hai ordinato tot prezzo</span>
+                </div>
+                <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Visualizza carrello</button>
+            </div>
+        </div>
+    </div>
+    <!-- Carrello off canvas -->
+    <div class="offcanvas offcanvas-bottom h-100 d-block d-sm-none overflow-y-auto" tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasBottomLabel">Il tuo carrello</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-4 col-lg-3 mx-2 my-2 card p-3 bg-alert">
+                        <div class="card border-0">
+                            <div class="card-header p-0 border-0">
+                                <img src="../assets/bacon.jpg" class="img-fluid rounded-top-1" alt="">
+                            </div>
+                            <div class="card-body p-3 rounded-bottom-1 border-0">
+                                <p>Titolo piatto</p>
+                                <p>Descrizione piatto</p>
+                                <p>Prezzo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3 mx-2 my-2 card p-3 bg-alert">
+                        <div class="card border-0">
+                            <div class="card-header p-0 border-0">
+                                <img src="../assets/bacon.jpg" class="img-fluid rounded-top-1" alt="">
+                            </div>
+                            <div class="card-body p-3 rounded-bottom-1 border-0">
+                                <p>Titolo piatto</p>
+                                <p>Descrizione piatto</p>
+                                <p>Prezzo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3 mx-2 my-2 card p-3 bg-alert">
+                        <div class="card border-0">
+                            <div class="card-header p-0 border-0">
+                                <img src="../assets/bacon.jpg" class="img-fluid rounded-top-1" alt="">
+                            </div>
+                            <div class="card-body p-3 rounded-bottom-1 border-0">
+                                <p>Titolo piatto</p>
+                                <p>Descrizione piatto</p>
+                                <p>Prezzo</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3 mx-2 my-2 card p-3 bg-alert">
+                        <div class="card border-0">
+                            <div class="card-header p-0 border-0">
+                                <img src="../assets/bacon.jpg" class="img-fluid rounded-top-1" alt="">
+                            </div>
+                            <div class="card-body p-3 rounded-bottom-1 border-0">
+                                <p>Titolo piatto</p>
+                                <p>Descrizione piatto</p>
+                                <p>Prezzo</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <button class="btn btn-primary p-3 w-100 mt-3">
+                            Effettua pagamento
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
     
 
 </template>
